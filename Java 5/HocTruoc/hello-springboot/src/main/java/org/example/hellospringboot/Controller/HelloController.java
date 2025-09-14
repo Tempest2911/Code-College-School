53dd1aef023a4b11aefd61a07ff569482d7db478
Tempest <dragonroblox2k7@gmail.com>
2025-09-14 09:12:22 +0700

    Auto commit Sun 09/14/2025  9:12:21.93

--- Java 5/HocTruoc/hello-springboot/src/main/java/org/example/hellospringboot/Controller/HelloController.java
+++ Java 5/HocTruoc/hello-springboot/src/main/java/org/example/hellospringboot/Controller/HelloController.java
@@ -2,13 +2,26 @@ package org.example.hellospringboot.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-@RequestMapping
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
 @Controller
 public class HelloController {
-    @RequestMapping("/hello")
+
+    @GetMapping("/hello")
     public String sayHello(Model model) {
         model.addAttribute("subject", "Spring Boot MVC");
-        return "demo/hello"; // This refers to hello.html in src/main/resources/templates/
+        model.addAttribute("author", "Nguyễn Văn A");
+        model.addAttribute("year", 2025);
+        return "demo/hello";
     }
+
+    @GetMapping("/students")
+    public String listStudents(Model model) {
+        List<String> lists = List.of("Nguyễn Văn A", "Trần Thị B", "Lê Văn C");
+        model.addAttribute("students", lists);
+        return "demo/hello";
+    }
+
 }
