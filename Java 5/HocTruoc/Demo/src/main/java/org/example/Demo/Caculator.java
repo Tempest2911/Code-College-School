9105a1572bafb04327d955ffcdd072f9004b99d0
Tempest <dragonroblox2k7@gmail.com>
2025-09-17 19:45:16 +0700

    Auto commit Wed 09/17/2025 19:45:16.19

--- Java 5/HocTruoc/Demo/src/main/java/org/example/Demo/Caculator.java
+++
@@ -1,7 +0,0 @@
-package org.example.Demo;
-
-public class Caculator {
-    public int add(int a, int b) {
-        return a + b;
-    }
-}
---
+++ Java 5/HocTruoc/hello-springboot/src/main/java/org/example/hellospringboot/Controller/MayTinhController.java
@@ -0,0 +1,45 @@
+package org.example.hellospringboot.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MayTinhController {
+    @GetMapping("may-tinh/cong")
+    @ResponseBody
+        public String mayTinh1(@RequestParam(name = "soThuNhat") int a, @RequestParam(name = "soThuHai") int b) {
+            int c = a+b;
+            return "ketqua = " +c;
+        }
+
+    @GetMapping("may-tinh/tru")
+    @ResponseBody
+    public String mayTinh2(@RequestParam(name = "soThuNhat") int a, @RequestParam(name = "soThuHai") int b) {
+        int c = a-b;
+        return "ketqua = " +c;
+    }
+
+    @GetMapping("may-tinh/nhan")
+    @ResponseBody
+    public String mayTinh3(@RequestParam(name = "soThuNhat") int a, @RequestParam(name = "soThuHai") int b) {
+
+        int c = a*b;
+        return "ketqua = " +c;
+    }
+
+    @GetMapping("may-tinh/chia")
+    @ResponseBody
+    public String mayTinh4(@RequestParam(name = "soThuNhat") double a, @RequestParam(name = "soThuHai", defaultValue = "0") double b) {
+
+        double c = a/b;
+        if (b==0) {
+            return "khong the chia het";
+        }else{
+            return "ketqua = " +c;
+        }
+
+    }
+
+}
