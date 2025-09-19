52580a45def42b641517655901f1c31190414ff4
Tempest <dragonroblox2k7@gmail.com>
2025-09-19 19:19:58 +0700

    Auto commit Fri 09/19/2025 19:19:58.62

--- Java 5/Lab2/src/main/java/org/example/lab2/Bai1Controller.java
+++ Java 5/Lab2/src/main/java/org/example/lab2/Bai1Controller.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class Bai1Controller {
     @GetMapping("lab-1/gpa")
     @ResponseBody
-    public String mayTinh1(@RequestParam(name = "lab") double a, @RequestParam(name = "quiz") double b, @RequestParam(name = "assignment") double c) {
+    public String mayTinh1(@RequestParam(name = "lab") double a,
+                           @RequestParam(name = "quiz") double b,
+                           @RequestParam(name = "assignment") double c) {
         double d = (a*0.26) + (b*0.14) + (c*0.2);
         if (a < 0 || a > 10 || b < 0 || b > 10 || c < 0 || c > 10) {
             return "Diem khong hop le";
