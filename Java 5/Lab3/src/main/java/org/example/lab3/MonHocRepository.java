2400950c131b52a915486146b348c5d9a82bfef4
Tempest <dragonroblox2k7@gmail.com>
2025-09-26 09:49:34 +0700

    Auto commit Fri 09/26/2025  9:49:33.90

--- Java 5/Lab3/src/main/java/org/example/lab3/MonHocRepository.java
+++ Java 5/Lab3/src/main/java/org/example/lab3/MonHocRepository.java
@@ -8,7 +8,7 @@ public class MonHocRepository {
     private static List<MonHoc> monHocs = new ArrayList<>();
     static {
         monHocs.add(new MonHoc("MH001", "Buôn bán máy bay", 4, "Digital Marketing", List.of("MinhDQ8"), true));
-        monHocs.add(new MonHoc("MH002", "Lập Trình Java", 3, "Công Nghệ Thông Tin", List.of("DungNA29"), true));
+        monHocs.add(new MonHoc("MH002", "Lập Trình Java 5", 3, "Công Nghệ Thông Tin", List.of("DungNA29"), true));
         monHocs.add(new MonHoc("MH003", "Vẽ SIGMA", 3, "Thiết kế đồ họa", List.of("TienNH21"), false));
         monHocs.add(new MonHoc("MH004", "Ferrari", 3, "Ô tô cơ khí", List.of("NguyenVV4"), false));
     }
